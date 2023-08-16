2bbf8a084366fdd3a296831972ffc802ff53b1a2
ZeYanG <35622268+ZeYanGzzz@users.noreply.github.com>
2023-08-16 17:34:43 +0800

    Add files via upload

---
+++ questionnaire/src/test/java/com/ydd/questionnaire/QuestionnaireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydd.questionnaire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
